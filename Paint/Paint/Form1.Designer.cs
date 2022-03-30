940c73d073af4033591fee9b51c4407a37677323
baariskose <61475377+baariskose@users.noreply.github.com>
2022-03-31 01:19:39 +0300

    Last

---
+++ Paint/Paint/Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace Paint
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnResimYükle = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.penSize = new System.Windows.Forms.NumericUpDown();
+            this.btnTemizle = new System.Windows.Forms.Button();
+            this.renkSec = new System.Windows.Forms.Button();
+            this.btnCizgi = new System.Windows.Forms.Button();
+            this.btnKaydet = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btnSilgi = new System.Windows.Forms.Button();
+            this.btnKalem = new System.Windows.Forms.Button();
+            this.btnDoldur = new System.Windows.Forms.Button();
+            this.btnRenkSec = new System.Windows.Forms.Button();
+            this.picBox = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.penSize)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.panel1.Controls.Add(this.btnResimYükle);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.penSize);
+            this.panel1.Controls.Add(this.btnCizgi);
+            this.panel1.Controls.Add(this.btnKaydet);
+            this.panel1.Controls.Add(this.btnTemizle);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.btnSilgi);
+            this.panel1.Controls.Add(this.btnKalem);
+            this.panel1.Controls.Add(this.btnDoldur);
+            this.panel1.Controls.Add(this.btnRenkSec);
+            this.panel1.Controls.Add(this.renkSec);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(862, 108);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnResimYükle
+            // 
+            this.btnResimYükle.BackColor = System.Drawing.Color.Moccasin;
+            this.btnResimYükle.Font = new System.Drawing.Font("Comic Sans MS", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnResimYükle.Location = new System.Drawing.Point(641, 22);
+            this.btnResimYükle.Name = "btnResimYükle";
+            this.btnResimYükle.Size = new System.Drawing.Size(66, 54);
+            this.btnResimYükle.TabIndex = 12;
+            this.btnResimYükle.Text = "Resim Sec";
+            this.btnResimYükle.UseVisualStyleBackColor = false;
+            this.btnResimYükle.Click += new System.EventHandler(this.btnResimYükle_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Bookman Old Style", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(511, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 18);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Kalem Boyutu:";
+            // 
+            // penSize
+            // 
+            this.penSize.Location = new System.Drawing.Point(514, 42);
+            this.penSize.Name = "penSize";
+            this.penSize.Size = new System.Drawing.Size(67, 22);
+            this.penSize.TabIndex = 10;
+            this.penSize.ValueChanged += new System.EventHandler(this.penSize_ValueChanged);
+            // 
+            // btnTemizle
+            // 
+            this.btnTemizle.BackColor = System.Drawing.Color.Moccasin;
+            this.btnTemizle.BackgroundImage = global::Paint.Properties.Resources.cleaning;
+            this.btnTemizle.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnTemizle.Location = new System.Drawing.Point(779, 22);
+            this.btnTemizle.Name = "btnTemizle";
+            this.btnTemizle.Size = new System.Drawing.Size(58, 54);
+            this.btnTemizle.TabIndex = 7;
+            this.btnTemizle.UseVisualStyleBackColor = false;
+            this.btnTemizle.Click += new System.EventHandler(this.btnTemizle_Click);
+            // 
+            // renkSec
+            // 
+            this.renkSec.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.renkSec.Location = new System.Drawing.Point(12, 33);
+            this.renkSec.Name = "renkSec";
+            this.renkSec.Size = new System.Drawing.Size(36, 31);
+            this.renkSec.TabIndex = 0;
+            this.renkSec.UseVisualStyleBackColor = false;
+            // 
+            // btnCizgi
+            // 
+            this.btnCizgi.BackColor = System.Drawing.Color.Moccasin;
+            this.btnCizgi.BackgroundImage = global::Paint.Properties.Resources.diagonal_line;
+            this.btnCizgi.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnCizgi.Location = new System.Drawing.Point(313, 23);
+            this.btnCizgi.Name = "btnCizgi";
+            this.btnCizgi.Size = new System.Drawing.Size(60, 49);
+            this.btnCizgi.TabIndex = 9;
+            this.btnCizgi.UseVisualStyleBackColor = false;
+            this.btnCizgi.Click += new System.EventHandler(this.btnCizgi_Click);
+            // 
+            // btnKaydet
+            // 
+            this.btnKaydet.BackColor = System.Drawing.Color.Moccasin;
+            this.btnKaydet.BackgroundImage = global::Paint.Properties.Resources.diskette;
+            this.btnKaydet.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnKaydet.Location = new System.Drawing.Point(713, 20);
+            this.btnKaydet.Name = "btnKaydet";
+            this.btnKaydet.Size = new System.Drawing.Size(60, 54);
+            this.btnKaydet.TabIndex = 8;
+            this.btnKaydet.UseVisualStyleBackColor = false;
+            this.btnKaydet.Click += new System.EventHandler(this.btnKaydet_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Moccasin;
+            this.button3.BackgroundImage = global::Paint.Properties.Resources.rectangle;
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button3.Location = new System.Drawing.Point(445, 23);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(60, 50);
+            this.button3.TabIndex = 6;
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Moccasin;
+            this.button2.BackgroundImage = global::Paint.Properties.Resources.oval;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button2.Image = global::Paint.Properties.Resources.dry_clean__1_;
+            this.button2.Location = new System.Drawing.Point(379, 23);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(64, 49);
+            this.button2.TabIndex = 5;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnSilgi
+            // 
+            this.btnSilgi.BackColor = System.Drawing.Color.Moccasin;
+            this.btnSilgi.BackgroundImage = global::Paint.Properties.Resources.eraser__1_;
+            this.btnSilgi.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnSilgi.Location = new System.Drawing.Point(247, 21);
+            this.btnSilgi.Name = "btnSilgi";
+            this.btnSilgi.Size = new System.Drawing.Size(60, 51);
+            this.btnSilgi.TabIndex = 4;
+            this.btnSilgi.UseVisualStyleBackColor = false;
+            this.btnSilgi.Click += new System.EventHandler(this.btnSilgi_Click);
+            // 
+            // btnKalem
+            // 
+            this.btnKalem.BackColor = System.Drawing.Color.Moccasin;
+            this.btnKalem.BackgroundImage = global::Paint.Properties.Resources.pencil;
+            this.btnKalem.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnKalem.Location = new System.Drawing.Point(181, 21);
+            this.btnKalem.Name = "btnKalem";
+            this.btnKalem.Size = new System.Drawing.Size(60, 51);
+            this.btnKalem.TabIndex = 3;
+            this.btnKalem.UseVisualStyleBackColor = false;
+            this.btnKalem.Click += new System.EventHandler(this.btnKalem_Click);
+            // 
+            // btnDoldur
+            // 
+            this.btnDoldur.BackColor = System.Drawing.Color.Moccasin;
+            this.btnDoldur.BackgroundImage = global::Paint.Properties.Resources.fill;
+            this.btnDoldur.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnDoldur.Location = new System.Drawing.Point(115, 21);
+            this.btnDoldur.Name = "btnDoldur";
+            this.btnDoldur.Size = new System.Drawing.Size(60, 51);
+            this.btnDoldur.TabIndex = 2;
+            this.btnDoldur.UseVisualStyleBackColor = false;
+            this.btnDoldur.Click += new System.EventHandler(this.btnDoldur_Click);
+            // 
+            // btnRenkSec
+            // 
+            this.btnRenkSec.BackColor = System.Drawing.Color.Moccasin;
+            this.btnRenkSec.BackgroundImage = global::Paint.Properties.Resources.color_palette;
+            this.btnRenkSec.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnRenkSec.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.btnRenkSec.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRenkSec.Location = new System.Drawing.Point(54, 22);
+            this.btnRenkSec.Name = "btnRenkSec";
+            this.btnRenkSec.Size = new System.Drawing.Size(55, 51);
+            this.btnRenkSec.TabIndex = 1;
+            this.btnRenkSec.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnRenkSec.UseVisualStyleBackColor = false;
+            this.btnRenkSec.Click += new System.EventHandler(this.btnRenkSec_Click);
+            // 
+            // picBox
+            // 
+            this.picBox.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.picBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.picBox.Location = new System.Drawing.Point(0, 0);
+            this.picBox.Name = "picBox";
+            this.picBox.Size = new System.Drawing.Size(862, 503);
+            this.picBox.TabIndex = 1;
+            this.picBox.TabStop = false;
+            this.picBox.Paint += new System.Windows.Forms.PaintEventHandler(this.picBox_Paint);
+            this.picBox.MouseClick += new System.Windows.Forms.MouseEventHandler(this.picBox_MouseClick);
+            this.picBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.picBox_MouseDown);
+            this.picBox.MouseMove += new System.Windows.Forms.MouseEventHandler(this.picBox_MouseMove);
+            this.picBox.MouseUp += new System.Windows.Forms.MouseEventHandler(this.picBox_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(862, 503);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.picBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximumSize = new System.Drawing.Size(1500, 1500);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Paint";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.penSize)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnRenkSec;
+        private System.Windows.Forms.Button renkSec;
+        private System.Windows.Forms.PictureBox picBox;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnSilgi;
+        private System.Windows.Forms.Button btnKalem;
+        private System.Windows.Forms.Button btnDoldur;
+        private System.Windows.Forms.Button btnTemizle;
+        private System.Windows.Forms.Button btnKaydet;
+        private System.Windows.Forms.Button btnCizgi;
+        private System.Windows.Forms.NumericUpDown penSize;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnResimYükle;
+    }
+}
+
---
+++ Paint/Paint/Form1.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Paint
+{
+    public partial class Form1 : Form
+    {
+        Bitmap bitmap;
+        Graphics graphics;
+        bool isPaint = false;
+        Point _x, _y;
+        Pen pen = new Pen(Color.Black, 1);
+        Pen silgi = new Pen(Color.White, 10);
+        int secilen;
+        int x, y, sX, sY, cX, cY;
+        ColorDialog colorDialog = new ColorDialog();
+        Color güncelRenk;
+        Size firstSize;
+        public Form1()
+        {
+            InitializeComponent();
+             firstSize = this.Size;
+            bitmap = new Bitmap(picBox.Width, picBox.Height);
+           
+            graphics = Graphics.FromImage(bitmap);
+            graphics.Clear(Color.White);
+         
+            picBox.Image = bitmap;
+          
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            secilen = 3;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            secilen = 4;
+        }
+
+        private void picBox_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics graphics = e.Graphics;
+            if (isPaint)
+            {
+                if (secilen == 3)
+                {
+                    graphics.DrawEllipse(pen, cX, cY, sX, sY);
+                }
+                if (secilen == 4)
+                {
+                    graphics.DrawRectangle(pen, cX, cY, sX, sY);
+                }
+                if (secilen == 5)
+                {
+                    graphics.DrawLine(pen, cX, cY, x, y);
+                    
+                }
+            }
+        }
+
+        private void btnTemizle_Click(object sender, EventArgs e)
+        {
+            graphics.Clear(Color.White);
+            picBox.Image = bitmap;
+            this.Size = firstSize;           
+            secilen = 0;
+        }
+
+        private void btnRenkSec_Click(object sender, EventArgs e)
+        {
+            colorDialog.ShowDialog();
+            güncelRenk = colorDialog.Color;
+            renkSec.BackColor = güncelRenk;
+            pen.Color = güncelRenk;
+        }
+
+        private void btnDoldur_Click(object sender, EventArgs e)
+        {
+            secilen = 7;
+        }
+
+        private void btnKaydet_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Png Files (*png) | *.png";
+            saveFileDialog.DefaultExt = "png";
+            saveFileDialog.AddExtension = true; 
+            if(saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                bitmap.Save(saveFileDialog.FileName, System.Drawing.Imaging.ImageFormat.Png);
+            }
+        }
+
+        private void btnCizgi_Click(object sender, EventArgs e)
+        {
+            secilen = 5;
+        }
+
+        private void penSize_ValueChanged(object sender, EventArgs e)
+        {
+            pen.Width = Convert.ToInt32(penSize.Value);
+        }
+
+        private void btnResimYükle_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if(openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                Bitmap original = new Bitmap(openFileDialog.FileName);
+                this.Size = original.Size;
+                bitmap = new Bitmap(original, new Size(picBox.Size.Width , picBox.Size.Height));
+                graphics = Graphics.FromImage(bitmap);
+                picBox.Image = bitmap;
+            }
+        }
+
+        private void picBox_MouseDown(object sender, MouseEventArgs e)
+        {
+            isPaint = true;
+            _y = e.Location;
+            cY = e.Y;
+            cX = e.X;
+        }
+
+        private void picBox_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isPaint)
+            {
+                 if(secilen == 1)
+                {
+                    _x = e.Location;
+                    graphics.DrawLine(pen, _x, _y);
+                    _y = _x;
+                }
+                if (secilen == 2)
+                {
+                    _x = e.Location; 
+                    graphics.DrawLine(silgi, _x, _y);
+                    _y = _x;
+                }
+            }
+            picBox.Refresh();
+            x = e.X;
+            y = e.Y;
+            sX = e.X - cX;
+            sY = e.Y - cY;
+        }
+        static Point set_point(PictureBox pb , Point pt)
+        {
+            float pX = 1f * pb.Image.Width / pb.Width;
+            float pY = 1f * pb.Image.Height / pb.Height;
+            return new Point((int)(pt.X * pX), (int)(pt.Y * pY));
+        }
+        private void picBox_MouseClick(object sender, MouseEventArgs e)
+        {
+            if(secilen ==7)
+            {
+                Point point = set_point(picBox, e.Location);
+                Doldur(bitmap, point.X, point.Y, güncelRenk);
+            }
+        }
+
+        private void picBox_MouseUp(object sender, MouseEventArgs e)
+        {
+            isPaint = false;
+
+            sX = x - cX;
+            sY = y - cY;
+            if(secilen == 3)
+            {
+                graphics.DrawEllipse(pen, cX, cY, sX, sY);
+            }
+            if (secilen == 4)
+            {
+                graphics.DrawRectangle(pen, cX, cY, sX, sY);
+            }
+            if (secilen == 5)
+            {
+                graphics.DrawLine(pen, cX, cY, x, y);
+            }
+        }
+
+        private void btnKalem_Click(object sender, EventArgs e)
+        {
+            secilen = 1;
+        }
+        private void validate(Bitmap bitmap, Stack<Point> stackPoint,int x , int y , Color previous_color, Color new_color)
+        {
+            Color ekrandanAlinan = bitmap.GetPixel(x, y);
+            if(ekrandanAlinan == previous_color)
+            {
+                stackPoint.Push(new Point(x, y)); 
+                bitmap.SetPixel(x, y, new_color);
+            }
+        }
+        public void Doldur(Bitmap bitmap , int x , int y, Color new_color)
+        {
+            Color pre_color = bitmap.GetPixel(x, y);
+            Stack<Point> pixel = new Stack<Point>();
+            pixel.Push(new Point(x, y));
+            bitmap.SetPixel(x, y, new_color);
+            if (pre_color == new_color)
+                return;
+            while(pixel.Count > 0)
+            {
+                Point pt = (Point)pixel.Pop();
+                if(pt.X>0 && pt.Y >0 && pt.X < bitmap.Width-1 && pt.Y <bitmap.Height - 1)
+                {
+                    validate(bitmap, pixel, pt.X-1, pt.Y, pre_color, new_color);
+                    validate(bitmap, pixel, pt.X, pt.Y-1, pre_color, new_color);
+                    validate(bitmap, pixel, pt.X+1, pt.Y, pre_color, new_color);
+                    validate(bitmap, pixel, pt.X, pt.Y+1, pre_color, new_color);
+                }
+            }
+        }
+        private void btnSilgi_Click(object sender, EventArgs e)
+        {
+            secilen = 2;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+          
+        }
+    }
+}
